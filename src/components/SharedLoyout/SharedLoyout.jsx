e5382b4710499b1913fdf52198b237f3176f849f
Michał Jarek <miciu77@gmail.com>
2023-01-25 20:00:25 +0100

    SharedLoyout styled

--- src/components/SharedLoyout/SharedLoyout.jsx
+++ src/components/SharedLoyout/SharedLoyout.jsx
@@ -1,8 +1,30 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, NavLink } from 'react-router-dom';
+import scss from './SharedLoyout.module.scss';
 
 export const SharedLayout = () => (
   <>
-        <nav>navigacja</nav>
+    <header className={scss.page_header}>
+      <div className={scss.container}>
+        <nav className={scss.header_navigation}>
+          <ul className={scss.header_navigation__list}>
+            <li>
+              <NavLink className={scss.header_navigation__item} to="/">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className={scss.header_navigation__item} to="/movies">
+                Movies
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <body>
+      <div className={scss.container}>
         <Outlet />
+      </div>
+    </body>
   </>
 );
